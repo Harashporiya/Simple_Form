d25ba8cdf0a54d4b44daa430965aee157a62238a
Your Name <your_email@example.com>
2024-01-22 13:58:25 +0530

    form

--- src/App.jsx
+++ src/App.jsx
@@ -14,40 +14,30 @@ export default function Form(){
         setLastName(e.target.value);
     }
     function phoneNumberHandel(e){
-    setPhoneNumber(e.target.value);
+           setPhoneNumber(e.target.value);
     }
     function emailHandel(e){
-    setEmail(e.target.value);
+         setEmail(e.target.value);
     }
 
     function handelReset(){
-        setFirstName('');
-        setLastName('');
-        setEmail('');
-        setPhoneNumber('');
+        window.location.reload();
+       
     }
     return(<>
     <h1 id="text">Simple Form</h1>
        <form id="form" >
 
-        <input type="text" placeholder="Enter the first name"
-        value={firstName}
-        onChange={firstNameHandel}/>
+        <input type="text" placeholder="Enter the first name" value={firstName} onChange={firstNameHandel}/>
          <br/>
          <br/>
-        <input type="text" placeholder="Enter the last name"
-        value={lastName}
-        onChange={lastNameHandel}/>
+        <input type="text" placeholder="Enter the last name"  value={lastName} onChange={lastNameHandel}/>
           <br/>
          <br/>
-         <input type="text" placeholder="Enter the phone number"  minLength={0} maxLength={10}
-        value={phoneNumber}
-        onChange={phoneNumberHandel}/>
+         <input type="text" placeholder="Enter the phone number"  minLength={0} maxLength={10}  value={phoneNumber} onChange={phoneNumberHandel}/>
         <br/>
         <br/>
-        <input type="text" placeholder="Enter email Id"
-        value={email}
-        onChange={emailHandel}/>
+        <input type="text" placeholder="Enter email Id" value={email} onChange={emailHandel}/>
 
         <h1>Name: {firstName} {lastName}</h1>
         <h1>Phone Number: {phoneNumber}</h1>
